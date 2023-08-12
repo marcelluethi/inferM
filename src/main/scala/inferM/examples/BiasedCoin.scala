b7048e9b61cfd9cefca302f153e4357e543d7b6e
Marcel Luethi <marcel.luethi@unibas.ch>
2023-08-12 10:36:38 +0200

    add biased coin example

---
+++ src/main/scala/inferM/examples/BiasedCoin.scala
@@ -0,0 +1,47 @@
+package inferM.examples
+
+import inferM.*
+import inferM.dists.*
+import inferM.sampler.*
+
+
+import scalagrad.api.spire.trig.DualScalarIsTrig.given
+import spire.implicits.DoubleAlgebra 
+
+import breeze.stats.{distributions => bdists}
+import breeze.stats.distributions.Rand.FixedSeed.randBasis
+
+import scalagrad.auto.forward.breeze.DeriverBreezeDoubleForwardPlan
+import scalagrad.auto.forward.breeze.DeriverBreezeDoubleForwardPlan.given
+import DeriverBreezeDoubleForwardPlan.{algebraT as alg}
+
+object BiasCoin extends App:  
+
+  // example data
+  val pGroundTruth = 0.1
+  val data = bdists.Bernoulli(pGroundTruth).sample(100)
+
+  val prior = for  
+    p <- RV.fromPrimitive(Uniform(alg.liftToScalar(0.0), alg.liftToScalar(1.0)), "p")
+  yield p
+
+  
+  val likelihood =  (p : Double) => 
+    val targetDist = Bernoulli(alg.liftToScalar(p))
+    
+    data.foldLeft(alg.zeroScalar)((sum, dataPoint) =>
+      sum + targetDist.logPdf(dataPoint)
+    )
+    
+  val posterior = prior.condition(likelihood)
+
+  // Sampling
+  val hmc = HMC[Double](
+    initialValue = Map("p" -> 0.5),
+    epsilon = 0.05,
+    numLeapfrog = 20
+  )
+
+  val samples = posterior.sample(hmc).take(1000).toSeq
+  println("mean p: " +samples.sum / samples.size)
+
