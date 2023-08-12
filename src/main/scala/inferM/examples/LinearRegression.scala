1ea8642bcaf5b989fd1e64dd40e09d60ae3d2b1c
Marcel Luethi <marcel.luethi@unibas.ch>
2023-08-12 05:52:22 +0200

    clean up regression example

--- src/main/scala/inferM/examples/LinearRegression.scala
+++ src/main/scala/inferM/examples/LinearRegression.scala
@@ -19,40 +19,40 @@ object LinearRegression extends App:
 
   // example data
   val aGroundTruth = 3.0
-  val bGroundTruth = 10.0
+  val bGroundTruth = 2.0
   val noiseSigma = 1.0
 
   val data = Seq.range(0, 10).map(x => 
     (x.toDouble, aGroundTruth * x + bGroundTruth + bdists.Gaussian(0.0, noiseSigma).sample())
   )
 
-  // defining the regression model 
+  case class Parameters(a : Double, b : Double)
+
   val prior = for  
     a <- RV.fromPrimitive(Gaussian(alg.liftToScalar(1.0), alg.liftToScalar(10)), "a")
     b <- RV.fromPrimitive(Gaussian(alg.liftToScalar(2.0), alg.liftToScalar(10)), "b")
-  yield (a, b)
+  yield Parameters(a, b)
 
-  def addPoint(x : Double, y : Double)(prior : RV[(Double, Double)]) : RV[(Double, Double)] = 
-    prior.condition((a, b) => 
-      Gaussian(alg.liftToScalar(a)  * alg.liftToScalar(x) +alg.liftToScalar(b), alg.liftToScalar(1.0)).logPdf(alg.liftToScalar(y)))
   
-  val likelihoods= data.map(point => (ab : Tuple2[Double, Double]) =>     
-    val (a, b) = ab
-    Gaussian(alg.liftToScalar(a)  * alg.liftToScalar(point._1) +alg.liftToScalar(b), alg.liftToScalar(1.0)).logPdf(alg.liftToScalar(point._2))
-  )
-  
+  val likelihood =  (parameters : Parameters) => 
+    data.foldLeft(alg.zeroScalar)((sum, point) =>
+      val (x, y) = point 
+      sum + Gaussian(
+        alg.liftToScalar(parameters.a)  * alg.liftToScalar(x) +alg.liftToScalar(parameters.b), 
+        alg.liftToScalar(1.0)).logPdf(alg.liftToScalar(y)
+      )
+    )
 
-  val posterior = data.foldLeft(prior)((prior, point) => addPoint(point._1, point._2)(prior))
-  val posterior2 = prior.conditionAll(likelihoods)
+  val posterior = prior.condition(likelihood)
 
   // Sampling
-  val hmc = HMC[(Double, Double)](
+  val hmc = HMC[Parameters](
     initialValue = Map("a" -> 0.0, "b" -> 0.0),
     epsilon = 0.5,
     numLeapfrog = 20
   )
 
-  val samples = posterior.sample(hmc).take(100000).toSeq
+  val samples = posterior.sample(hmc).take(10000).toSeq
 
   println("mean a: " +samples.map(_._1).sum / samples.size)
   println("mean b: " +samples.map(_._2).sum / samples.size)
