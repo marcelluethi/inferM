34df2534a18008fe4a22fdbf1e933eff4ecc4ce0
Marcel Luethi <marcel.luethi@unibas.ch>
2023-08-14 18:02:02 +0200

    add scalar parameter to illustrate mixing scalar and vector parameters

--- src/main/scala/inferM/examples/DensityEstimationMultivariate.scala
+++ src/main/scala/inferM/examples/DensityEstimationMultivariate.scala
@@ -20,26 +20,27 @@ import breeze.linalg.DenseMatrix
 object DensityEstimationMultiVariate extends App:  
 
   // example data
-  val muGroundTruth = DenseVector(1.0, 2.0)
-  val covGroundTruth = DenseMatrix((1.0, 0.0), (0.0, 1.0))
+  val muGroundTruth = DenseVector(0.0, 0.0)
+  val covGroundTruth = DenseMatrix((3.0, 0.0), (0.0, 3.0))
 
   val data = bdists.MultivariateGaussian(muGroundTruth, covGroundTruth).sample(100)
   
-  case class Parameters(mu : DenseVector[Double])
+  case class Parameters(mu : DenseVector[Double], sigma2 : Double)
 
   val prior = for  
     mu <- RV.fromPrimitiveMultivariate(
       MultivariateGaussian(
-        alg.lift(DenseVector.zeros[Double](2)), 
-        alg.lift(DenseMatrix.eye[Double](2))
+        alg.lift(DenseVector.ones[Double](2)), 
+        alg.lift(DenseMatrix.eye[Double](2) * 1.0)
       ), "mean")
-  yield Parameters(mu)
+    sigma2 <- RV.fromPrimitive(Exponential(alg.lift(0.1)), "sigma2")    
+  yield Parameters(mu, sigma2)
 
   
   val likelihood =  (parameters : Parameters) => 
     val targetDist = MultivariateGaussian(
         alg.lift(parameters.mu),
-        alg.lift(covGroundTruth)
+        alg.lift(DenseMatrix.eye[Double](2)) * alg.lift(parameters.sigma2)
     )
       
     data.foldLeft(alg.zeroScalar)((sum, point) =>
@@ -50,13 +51,16 @@ object DensityEstimationMultiVariate extends App:
 
   // Sampling
   val hmc = HMC[Parameters](
-    initialValue = Map("mean" -> DenseVector.zeros[Double](2)),
-    epsilon = 0.05,
+    initialValue = Map("mean" -> DenseVector.zeros[Double](2), "sigma2" -> 3.0),
+    epsilon = 0.1,
     numLeapfrog = 20
   )
 
-  val samples = posterior.sample(hmc).take(1000).toSeq
+  val samples = posterior.sample(hmc).drop(500).take(1000).toSeq
   val estimatedMean = samples.map(_.mu).reduce(_ + _) * (1.0 /  samples.size)
+  val estimatedSigma2 = samples.map(_.sigma2).reduce(_ + _) * (1.0 /  samples.size)
+  val varSigma2 = samples.map(_.sigma2).map(x => (x - estimatedSigma2) * (x - estimatedSigma2)).reduce(_ + _) * (1.0 /  samples.size)
   println("mean " + estimatedMean)
-
+  println("sigma2 " + estimatedSigma2)
+  println("var sigma2 " + varSigma2)
 
